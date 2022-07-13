6b25d8692acd88764a42689391c22577b3787b49
oseiasal <oseias.nascimento@kainos.com.br>
2022-07-13 13:28:22 -0300

    validação de contrato

--- cypress/integration/exercicio-api.spec.js
+++ cypress/integration/exercicio-api.spec.js
@@ -1,30 +1,34 @@
 /// <reference types="cypress" />
+import user from '../contracts/users.contract'
 
 describe('Testes da Funcionalidade Usuários', () => {
 
-    it('Deve validar contrato de usuários', () => {
-         //TODO: 
-    });
+     it('Deve validar contrato de usuários', () => {
+          cy.request('usuarios').then((response) => {
+               cy.log("Contrato validado")
+               return user.validateAsync(response.body)
+          })
+     });
 
-    it('Deve listar usuários cadastrados', () => {
-         //TODO: 
-    });
+     it('Deve listar usuários cadastrados', () => {
+          //TODO: 
+     });
 
-    it('Deve cadastrar um usuário com sucesso', () => {
-         //TODO: 
-    });
+     it('Deve cadastrar um usuário com sucesso', () => {
+          //TODO: 
+     });
 
-    it('Deve validar um usuário com email inválido', () => {
-         //TODO: 
-    });
+     it('Deve validar um usuário com email inválido', () => {
+          //TODO: 
+     });
 
-    it('Deve editar um usuário previamente cadastrado', () => {
-         //TODO: 
-    });
+     it('Deve editar um usuário previamente cadastrado', () => {
+          //TODO: 
+     });
 
-    it('Deve deletar um usuário previamente cadastrado', () => {
-        //TODO: 
-    });
+     it('Deve deletar um usuário previamente cadastrado', () => {
+          //TODO: 
+     });
 
 
 });
